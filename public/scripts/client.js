c5d46b445ba5fc2c046d5094f40cab5cdd8be82c
Dominic <nickrsd@gmail.com>
2019-05-02 18:10:34 -0700

    fixed endpoint

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //https://mighty-everglades-83549.herokuapp.com
 
-    var socket = io('localhost:8000/');
+    var socket = io('https://mighty-everglades-83549.herokuapp.com:8000/');
     //const ioClient = socket.connect("http://localhost:8000")
 
     //ioClient.on("seq-num", (msg) => console.info(msg))
