30c78fb3fc6e451f585e7ca7f7ad14862e6ba85a
Dominic <dominic.rodriquez@slickdeals.net>
2019-05-03 15:01:34 -0700

    removed https

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //https://mighty-everglades-83549.herokuapp.com
 
-    var socket = io.connect('https://www.thewatchful.net:8000/');
+    var socket = io.connect('http://www.thewatchful.net:8000/');
     //const ioClient = socket.connect("http://localhost:8000")
 
     //ioClient.on("seq-num", (msg) => console.info(msg))
