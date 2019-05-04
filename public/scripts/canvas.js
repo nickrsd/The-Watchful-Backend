9f95f6b7d6d895d1fadc767fb4a4a6bcbd536966
Dominic <dominic.rodriquez@slickdeals.net>
2019-05-03 19:44:14 -0700

    added a different joystick

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -1,26 +1,52 @@
-CanvasGamepad.setup(
-	{
-		canvas:"controller",
-		start:{name:"start", key:"b"},
-		select:{name:"select", key:"v"},
-		trace:true,
-		debug:true,
-		hint:true,
-		buttons:[
-			{name:"a", "key":"s"},
-			{name:"b", "key":"a"},
-			{name:"x", "key":"w"},
-			{name:"y", "key":"q"}
-		]      
-	}
-);
-multikey.setup(CanvasGamepad.events, "qwasbv", true);
 
-setInterval(
-    function()
-    {
-        var map = CanvasGamepad.observe();
-        //console.log(new Date() + ":" + JSON.stringify(map));
-    }
-    ,1000
-);
+var joystick
+
+var joysticks = {
+	dynamic: {
+		zone: document.querySelector('.zone.dynamic'),
+		color: 'blue',
+		multitouch: true
+	},
+	semi: {
+		zone: document.querySelector('.zone.semi'),
+		mode: 'semi',
+		catchDistance: 150,
+		color: 'white'
+	},
+	static: {
+		zone: document.querySelector('.zone.static'),
+		mode: 'static',
+		position: {
+		left: '50%',
+		top: '50%'
+		},
+		color: 'red'
+	}
+};
+
+function bindJoystick() {
+	joystick.on('start end', function(evt, data) {
+		console.log(data)
+	}).on('move', function(evt, data) {
+		console.log(data)
+	}).on('dir:up plain:up dir:left plain:left dir:down ' +
+			'plain:down dir:right plain:right',
+			function(evt, data) {
+		console.log(data)
+	}).on('pressure', function(evt, data) {
+		console.log(data)
+	});
+}
+
+function createJoystick(evt) {
+	var type = typeof evt === 'string' ?
+		evt : evt.target.getAttribute('data-type');
+	if (joystick) {
+		joystick.destroy();
+	}
+
+	joystick = nipplejs.create(joysticks[type]);
+	bindJoystick();
+}
+
+createJoystick('dynamic');
